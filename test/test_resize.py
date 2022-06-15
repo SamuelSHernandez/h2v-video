66e73f4544022f6edbd85ef701dae10c95232c48
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2022-06-14 21:25:36 -0500

    Resize test

--- test/test_resize.py
+++ test/test_resize.py
@@ -1,12 +1,17 @@
+from typing_extensions import assert_type
 from unittest import TestCase
 
 """
 Test
 """
-
+from app.resize import new_height, new_width
 class TestResize(TestCase):
     """
     Test
     """
+    def __init__(self):
+        self.new_height = 1080
+        self.new_width  = 520
 
-    assert True
+    TestCase.assertEqual(new_height, 1080)
+    TestCase.assertEqual(new_width, 520)    
